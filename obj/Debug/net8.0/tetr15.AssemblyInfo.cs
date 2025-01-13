88b04be2e20e3a1ebb8002ed035b06c1c11e7eab
R-Bet <roybetsalel@gmail.com>
2025-01-13 03:36:03 +0200

    New gitignore

--- obj/Debug/net8.0/tetr15.AssemblyInfo.cs
+++ obj/Debug/net8.0/tetr15.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tetr15")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d9f973968f959c0da79cbdeaa381779985d11b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2df018df4d52f1de8912875183acc7b310fa9907")]
 [assembly: System.Reflection.AssemblyProductAttribute("tetr15")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tetr15")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
